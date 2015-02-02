ceeeef0bc8766d95b6ff205c3529a41d636086a5
divyasreem <divya@mantralabsglobal.com>
2015-02-02 23:42:17 +0530

    Able to add new user

--- services/module/User/src/User/Controller/UserController.php
+++ services/module/User/src/User/Controller/UserController.php
@@ -23,4 +23,60 @@ class UserController extends AbstractRestfulJsonController{
         }, $users);
         return new JsonModel($users);
     }
+    
+    public function getList(){   
+        // Action used for GET requests without resource Id
+        $users = $this->getEntityManager()->getRepository('User\Entity\User')->findAll();
+        $users = array_map(function($user){
+            return $user->toArray();
+        }, $users);
+        return new JsonModel($users);
+    }
+
+    public function get($id){   
+        // Action used for GET requests with resource Id
+        $user = $this->getEntityManager()->getRepository('User\Entity\User')->find($id);
+        return new JsonModel(
+            $user->toArray()
+        );
+    }
+
+    public function getMyUsersAction(){
+        return $this->getList();
+    }
+
+    public function create($data){
+        // print_r($data);
+        $this->getEntityManager();
+        // print_r($data);
+        $user = new \User\Entity\User($data);
+        // print_r($user);
+        // $user->validate($this->em);
+        
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+        
+        return new JsonModel($user->toArray());
+    }
+
+    public function update($id, $data){
+        // Action used for PUT requests
+        $user = $this->getEntityManager()->getRepository('User\Entity\User')->find($id);
+        $user->set($data);
+        $user->validate($this->em);
+        
+        $this->getEntityManager()->flush();
+        
+        return new JsonModel($user->toArray());
+    }
+
+    public function delete($id){
+        // Action used for DELETE requests
+        $user = $this->getEntityManager()->getRepository('User\Entity\User')->find($id);
+        $this->getEntityManager()->remove($user);
+        
+        $this->getEntityManager()->flush();
+        
+        return new JsonModel($user->toArray());
+    }
 }
--- services/module/User/src/User/Entity/User.php
+++ services/module/User/src/User/Entity/User.php
@@ -5,8 +5,9 @@ use User\Entity\Base;
 
 use Zend\InputFilter\InputFilter;
 
+
 /** @ORM\Entity */
-class User
+class User  extends Base
 {
     /**
     * @ORM\Id
@@ -30,4 +31,113 @@ class User
      */
     protected $role;
     
+    /**
+     * Convert the object to an array.
+     *
+     * @return array
+     */
+    
+    public function __construct($data){
+        parent::__construct($data);
+    }
+
+    public function getFullName(){
+        return $this->fname . " " . $this->lname;
+    }
+
+    public function getAddress(){
+        return $this->address;
+    }
+
+    public function getInputFilter($em){
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+ 
+            $inputFilter->add(array(
+                'name'     => 'fname',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 5,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+ 
+            $inputFilter->add(array(
+                'name'     => 'lname',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+ 
+            $inputFilter->add(array(
+                'name'     => 'password',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+
+            $inputFilter->add(array(
+                'name'     => 'email',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'EmailAddress',
+                    ),
+                    array(
+                        'name'  => 'User\Validator\NoEntityExists',
+                        'options'=>array(
+                            'entityManager' =>$em,
+                            'class' => 'User\Entity\User',
+                            'property' => 'email',
+                            'exclude' => array(
+                                array('property' => 'id', 'value' => $this->getId())
+                            )
+                        )
+                    )
+                ),
+            ));
+ 
+            $this->inputFilter = $inputFilter;
+        }
+ 
+        return $this->inputFilter;
+    }
 }
