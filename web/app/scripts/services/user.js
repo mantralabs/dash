16ff9e2b0d8d85de9adca105a664f5b2ffc1edda
dhananjay-weds-vanishree <vani.jay26@gmail.com>
2015-04-02 19:57:03 +0530

    Added User.js back to repo

---
+++ web/app/scripts/services/user.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('pmtoolApp')
+  .service('Userservice', function Userservice($q, $http, $resource) {
+  	// console.log(data);
+
+    this.signupNewUser = function (userInfo) {
+  		return $http({
+  			// url : 'http://local.api.dash.com/user',
+        url : baseUrl+'user/',
+  			method : 'POST',
+  			dataType : 'json',
+    		contentType : 'application/json',
+    		data : JSON.stringify(userInfo)
+    	});
+    };
+	  this.signin = function (userInfo) {
+  		// var defer = $q.defer();
+  		// console.log(defer);
+  		// console.log(userInfo);
+  		return $http({
+  			// url : 'http://local.api.dash.com/user/login',
+        url :  baseUrl+'user/login',
+  			method : 'POST',
+  			dataType : 'json',
+    		contentType : 'application/json',
+    		data : JSON.stringify(userInfo)
+    	});
+    };
+	// this.users = $resource('http://localhost/responses/index.php',{},{'login':{'method':'get'}});
+	// this.users = $resource('http://localhost/responses/index.php',{},{'signup':{'method':'post'}});
+     this.signout = function() {
+      console.log('signout function is executed');
+      return $http({
+        url : 'http://local.api.dash.com/user/logout',
+        method : 'POST',
+      });
+    };
+
+    this.forgotPassword = function(){
+      console.log('forgot-password');
+      
+    }
+
+  });
