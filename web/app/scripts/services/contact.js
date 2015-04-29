cd6998ec0b7a0bfff5c4558ba25c8de2d4af195c
Rakesh <Rakesh@Rakesh-MacBook-Air.local>
2015-04-29 18:38:20 +0530

    small fix

--- web/app/scripts/services/contact.js
+++ web/app/scripts/services/contact.js
@@ -19,7 +19,7 @@ angular.module('pmtoolApp')
   	Contact.add = function(data, callback){
   		if(!Contact.list){
   			Contact.list = [];
-  		}
+  		} 
   		console.log(data);
   		$http.post('/api/user', data)
   		.success(function(user){
