ad3a1836ec3f6f5db33e4893e7248552d5e54c94
SYuvaraj <s.yuvaraj03@gmail.com>
2015-05-22 12:12:06 +0530

    changes in adding users for projects

--- web/app/scripts/controllers/project.js
+++ web/app/scripts/controllers/project.js
@@ -40,28 +40,46 @@ angular.module('pmtoolApp')
 
 	$scope.user = $rootScope.user;
 	$scope.userIds = [];
+
+	Project.fetchProject($routeParams.id).then(function(response){
+			$scope.project = response;
+		}).catch(function(err){
+			console.log(err);
+			$scope.error = err.message;
+		});
 	
 	//if user checked push id into array if uncheck remove from array.
 	$scope.sync = function(bool, item){
 	    if(bool){
 	      // add item
 	      $scope.userIds.push(item.id);
+	      console.log($scope.userIds);
 	    } else {
 	      // remove item
 	      console.log($scope.userIds.length);
 	      for(var i=0 ; i < $scope.userIds.length; i++) {
 	        if($scope.userIds[i] == item.id){
 	          $scope.userIds.splice(i,1);
+	          console.log($scope.userIds);
 	        }
-	      }      
+		  } 
+		   
 	    }
   	};
 
-  	//is checked method is used to send checkbox result true/false on checked for checkbox.
+  	$scope.itemsPush = function(){
+  		for(var i=0 ; i < $scope.project[0].users.length; i++){
+			 $scope.userIds.push($scope.project[0].users[i].id)
+			 console.log($scope.userIds);
+		}
+  	}
+
+  
   	$scope.isChecked = function(id){
-      var match = false;
-      for(var i=0 ; i < $scope.userIds.length; i++) {
-        if($scope.userIds[i].id == id){
+  	  var match = false;
+      for(var i=0 ; i < $scope.project[0].users.length; i++) {
+      	console.log("ischecked inside loop")
+        if($scope.project[0].users[i].id == id){
           match = true;
         }
       }
@@ -69,10 +87,9 @@ angular.module('pmtoolApp')
   	};
 
 	$scope.addMemberToProject = function(){
-		// console.log($scope.userIds);
 		$scope.projectId = $routeParams.id;
 		var data = {
-			"users" : $scope.userIds
+			"users" : $scope.userIds 
 		};
 
 		Project.addProjectMember($scope.projectId,data).then(function(response){
@@ -80,6 +97,7 @@ angular.module('pmtoolApp')
 			}).catch(function(err){
 				$scope.error = err.message;
 			});
+		
 	};
 
 	Project.fetchProject($routeParams.id).then(function(response){
